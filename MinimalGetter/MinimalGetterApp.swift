776b276ed172b2b751e4a14eeff37c51bed5c4fe
Stefan Gränitz <stefan.graenitz@gmail.com>
2024-12-17 11:13:17 +0100

    Initial Commit

---
+++ MinimalGetter/MinimalGetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalGetterApp.swift
+//  MinimalGetter
+//
+//  Created by Stefan Gränitz on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MinimalGetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
